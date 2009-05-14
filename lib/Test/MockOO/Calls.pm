fe05fcff6b62546500636bc198d382d4d356f6f1
Shawn M Moore <sartak@gmail.com>
2009-05-14 03:13:19 -0400

    Calls->called_ok

--- lib/Test/MockOO/Calls.pm
+++ lib/Test/MockOO/Calls.pm
@@ -4,14 +4,27 @@ use MooseX::AttributeHelpers;
 
 has calls => (
     metaclass => 'Collection::Array',
-    is        => 'ro',
     isa       => 'ArrayRef[Test::MockOO::Call]',
     default   => sub { [] },
     provides  => {
-        push  => '_add_call',
-        clear => 'clear',
+        push     => '_add_call',
+        clear    => 'clear',
+        elements => 'calls',
     },
 );
+
+sub called_ok {
+    my $self = shift;
+    my $name = shift;
+    my $desc = shift;
+
+    my $found = 0;
+    for my $call ($self->calls) {
+        $found = 1, last if $call->name eq $name;
+    }
+
+    Test::More::ok($found, $desc || "called the $name method");
+}
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
