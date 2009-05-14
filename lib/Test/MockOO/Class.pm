f847341e924d98dab361b8e62dd098b591409c50
Shawn M Moore <sartak@gmail.com>
2009-05-14 03:08:00 -0400

    Move Mocks into a role

--- lib/Test/MockOO/Class.pm
+++ lib/Test/MockOO/Class.pm
@@ -2,67 +2,7 @@ package Test::MockOO::Class;
 use Moose;
 
 extends 'Moose::Meta::Class';
-
-sub mock {
-    my $self = shift;
-    my $name = shift;
-    my $code = shift;
-
-    confess "Too many arguments for mock" if @_;
-
-    $self->add_method($name => $code);
-
-    return $self;
-}
-
-sub set_true {
-    my $self = shift;
-    my $name = shift;
-
-    confess "set_true does not work on multiple method names" if @_;
-
-    $self->mock($name => sub { 1 });
-}
-
-sub set_false {
-    my $self = shift;
-    my $name = shift;
-
-    confess "set_false does not work on multiple method names" if @_;
-
-    $self->mock($name => sub { return });
-}
-
-sub set_always {
-    my $self  = shift;
-    my $name  = shift;
-    my $value = shift;
-
-    confess "Too many values for set_always, which takes a scalar" if @_;
-
-    $self->mock($name => sub { $value });
-}
-
-sub set_series {
-    my $self   = shift;
-    my $name   = shift;
-    my @values = @_;
-
-    $self->mock($name => sub {
-        return shift @values if @values;
-        return;
-    });
-}
-
-sub set_list {
-    my $self = shift;
-    my $name = shift;
-    my @list = @_;
-
-    # Test::MockObject makes a shallow copy of @list. Not sure why, so I'm not
-    # going to cargo cult it.
-    $self->mock($name => sub { @list });
-}
+with 'Test::MockOO::Mocks';
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
