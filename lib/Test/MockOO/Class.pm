69d5c995c5ba8d3267086fc0f975f79d886ad163
Shawn M Moore <sartak@gmail.com>
2009-05-26 13:10:28 -0400

    Don't remove meta yet, needs a Moose improvement

--- lib/Test/MockOO/Class.pm
+++ lib/Test/MockOO/Class.pm
@@ -8,16 +8,6 @@ with (
     'Test::MockOO::CallTracing',
 );
 
-# purify mock class by removing the meta method that
-# Class::MOP::Class::create adds
-# FIXME: do we want to do this?
-sub create {
-    my ($self, $package_name, %options) = @_;
-    my $meta = $self->SUPER::create($package_name, %options);
-    $meta->remove_method('meta') unless $options{methods}{meta};
-    return $meta;
-}
-
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
